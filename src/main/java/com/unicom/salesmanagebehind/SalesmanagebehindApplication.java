fb74385896ec3ea5fe63ba710313d3d6c5e9ce45
ych94 <313980657@qq.com>
2019-05-21 14:25:28 +0800

    公告

--- src/main/java/com/unicom/salesmanagebehind/SalesmanagebehindApplication.java
+++ src/main/java/com/unicom/salesmanagebehind/SalesmanagebehindApplication.java
@@ -1,10 +1,12 @@
 package com.unicom.salesmanagebehind;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.unicom.salesmanagebehind.dao")
 public class SalesmanagebehindApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/unicom/salesmanagebehind/dao/noticeDao.java
+++ src/main/java/com/unicom/salesmanagebehind/dao/noticeDao.java
@@ -1,8 +1,16 @@
 package com.unicom.salesmanagebehind.dao;
 
 import com.unicom.salesmanagebehind.model.Notice;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
+@Mapper
 public interface noticeDao {
+    @Select("select * from notice")
+    List<Notice> getAllNotice();
     int deleteByPrimaryKey(Integer noticeId);
 
     int insert(Notice record);
