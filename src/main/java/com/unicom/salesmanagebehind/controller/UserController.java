a6ea68d2f61c51e4357b19da330073349088f6ce
cny <123456>
2019-05-28 11:19:37 +0800

    用户模块1.2

--- src/main/java/com/unicom/salesmanagebehind/controller/UserController.java
+++ src/main/java/com/unicom/salesmanagebehind/controller/UserController.java
@@ -7,6 +7,7 @@ import com.unicom.salesmanagebehind.model.User;
 import com.unicom.salesmanagebehind.service.UserService;
 import com.unicom.salesmanagebehind.utils.JSONResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,14 +21,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/list")
+    @GetMapping(value = "/list")
     public JSONResult getUserList(
             @RequestParam(value = "page",defaultValue = "1") int page,
-            @RequestParam(value = "limit",defaultValue = "5") int limit
+            @RequestParam(value = "limit",defaultValue = "10") int limit,
+            @RequestParam(name = "userName",defaultValue ="") String userName,
+            @RequestParam(name = "productName",defaultValue = "") String productName,
+            @RequestParam(name = "tel",defaultValue = "") String tel,
+            @RequestParam(name = "fee",defaultValue = "") Integer fee,
+            @RequestParam(name = "onlineTime",defaultValue = "") Integer onlineTime
     ){
         PageHelper.startPage(page,limit);
 
-        List<User> list = userService.getUserList();
+        List<User> list = userService.getUserList(userName,productName,tel,fee,onlineTime);
 
         PageInfo<User> pageInfo =new PageInfo<>(list);
 
