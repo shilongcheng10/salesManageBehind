d8176f30176cc9aa564e12db3fce2081f789409d
cny <123456>
2019-05-28 09:28:24 +0800

    用户模块1.1

--- src/main/java/com/unicom/salesmanagebehind/dao/UserDao.java
+++ src/main/java/com/unicom/salesmanagebehind/dao/UserDao.java
@@ -1,7 +1,6 @@
 package com.unicom.salesmanagebehind.dao;
 
 import com.unicom.salesmanagebehind.model.User;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
---
+++ src/main/java/com/unicom/salesmanagebehind/model/UserPojo.java
@@ -0,0 +1,155 @@
+package com.unicom.salesmanagebehind.model;
+
+import java.util.Date;
+
+public class UserPojo {
+    private int userId;
+
+    private String userName;
+
+    private int userSex;
+
+    private int userAge;
+
+    private Date birth;
+
+    private int productId;
+
+    private int fee;
+
+    private String tel;
+
+    private String status;
+
+    private Date activeDate;
+
+    private int onlineTime;
+
+    private int isCbss;
+
+    private String IDCard;
+
+    private String cardType;
+
+    private String productName;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(int userSex) {
+        this.userSex = userSex;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
+    public void setFee(int fee) {
+        this.fee = fee;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getActiveDate() {
+        return activeDate;
+    }
+
+    public void setActiveDate(Date activeDate) {
+        this.activeDate = activeDate;
+    }
+
+    public int getOnlineTime() {
+        return onlineTime;
+    }
+
+    public void setOnlineTime(int onlineTime) {
+        this.onlineTime = onlineTime;
+    }
+
+    public int getIsCbss() {
+        return isCbss;
+    }
+
+    public void setIsCbss(int isCbss) {
+        this.isCbss = isCbss;
+    }
+
+    public String getIDCard() {
+        return IDCard;
+    }
+
+    public void setIDCard(String IDCard) {
+        this.IDCard = IDCard;
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(String cardType) {
+        this.cardType = cardType;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
