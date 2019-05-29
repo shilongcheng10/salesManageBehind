a978ccdd1d401a2c6833bced74ec565c613550ba
shilongcheng10 <shilongcheng10@outlook.com>
2019-05-29 15:12:18 +0800

    adoc文档与html5文档生成

--- src/test/java/com/unicom/salesmanagebehind/SalesmanagebehindApplicationTests.java
+++ src/test/java/com/unicom/salesmanagebehind/SalesmanagebehindApplicationTests.java
@@ -1,19 +1,46 @@
 package com.unicom.salesmanagebehind;
 
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.asciidoctor.cli.AsciidoctorInvoker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URL;
+import java.nio.file.Paths;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SalesmanagebehindApplicationTests {
 
     @Test
-    public void contextLoads() {
-    }
+    public void generateAsciiDocs() throws Exception {
+        //    输出Ascii格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withGeneratedExamples()
+                .withMarkupLanguage(MarkupLanguage.ASCIIDOC)
+                .build();
 
+        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("src/docs/asciidoc/generated/api"));
+    }
+    @Test
+    public void generatePDF() {
+        //样式
+        String style = "pdf-style=E:\\themes\\theme.yml";
+        //字体
+        String fontsdir = "pdf-fontsdir=E:\\fonts";
+        //需要指定adoc文件位置
+        String adocPath = "E:\\all.adoc";
+        AsciidoctorInvoker.main(new String[]{"-a",style,"-a",fontsdir,"-b","pdf",adocPath});
+    }
 
 
 }
